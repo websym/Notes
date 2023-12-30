fc9b61630899322043e62203641e8c9724064f29
websym <szones333@gmail.com>
2023-12-30 18:15:17 +0545

    commit toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
-import axios from "axios"
 import { useEffect, useState } from "react"
-import Note from "./component"
+import axios from "axios"
+import Data1 from "./component";
 
 const App=()=>{
-const[notes,setNotes] =useState([
 
-])
+  const[notes,setNotes] =useState([]);
+ const[newNotes,setNewNotes] = useState([]);
 useEffect(()=>{
-  axios.get("http://localhost:3001/notes")
-  .then(res=>{
-    setNotes(res.data);
+  axios.get("http://localhost:3001/notes").then(res=>{
+    setNotes(res.data)
   })
-})
+});
+const handlechange = (event)=>{
+  setNewNotes(event.target.value)
+ }
+ const addNew=(event)=>{
+  event.preventDefault();
 
+  const addNewObj={
+    
+    id:notes.length+ 1,
+    title: newNotes,
+    body: '' , 
+    important: Math.random()>0.5
+    
+   }
+   
+   axios.post("http://localhost:3001/notes",addNewObj).then(res=>{
+    setNewNotes(notes.concat(res));
+    setNewNotes('');
+   })
+   
+  }
+  const toggleImportanceOf=id=>{
+    //console.log('toggleImportance',id);
+    
+    const url='http://localhost:3001/notes/${id}'
+    const note=notes.find(note=>id === note.id)
+    const changedNote={...notes,important: !note.important}
+    //console.log(changedNote)
+    axios.put(url,changedNote).then(res=>{
+      setNotes(notes.map(note => note.id !== id ? res.data : note))
+    })      
+   }
   return(
     <div>
-      {notes.map(note=><Note key={note.id}note={note}/>)}
+      <form onSubmit={addNew} >
+        <input value={newNotes} onChange={handlechange} />
+        <button>Add</button>
+      </form>
+     {
+      notes.map((note)=>{
+        return <Data1 note={note} key={note.id} toggleImportance={()=>{ toggleImportanceOf(note.id)}}/>
+      })
+     }
+   
     </div>
-    
   )
 }
 export default App
--- src/component.jsx
+++ src/component.jsx
@@ -1,9 +1,16 @@
-const Note=({note})=>{
+
+const Data1=({note,toggleImportance})=>{
+    const label=note.important ? 'make not important': 'make important';
+    
     return(
+        
         <div>
             <h1>{note.title}</h1>
-            <p>{note.body}</p>
+           <p>{note.body}</p>
+           <h2>{note.component}</h2>
+           <button onClick={()=>{toggleImportance(note.id)}}>{label}</button>
         </div>
+        
     )
 }
-export default Note
+export default Data1
